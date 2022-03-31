8a110e96d3d9ab6ace0c32323bae98b98a39cecc
Rob Baker <rob.baker@alanrogers.com>
2022-03-31 17:36:19 +0100

    Optimise twig globals

--- src/twig/TwigGlobals.php
+++ src/twig/TwigGlobals.php
@@ -8,30 +8,22 @@ use Twig\Extension\GlobalsInterface;
 
 class TwigGlobals extends AbstractExtension implements GlobalsInterface
 {
-    /**
-     * @var bool
-     */
-    private bool $is_console_request;
-
-    public function __construct()
-    {
-        $this->is_console_request = Craft::$app instanceof craft\console\Application;
-    }
-
     public function getGlobals() : array
     {
         $can_use_webp = true;
         $browser_type = 'modern';
 
-        if (!$this->is_console_request) {
+        if (!Craft::$app instanceof craft\console\Application) {
+
+            $request_headers = Craft::$app->getRequest()->getHeaders();
 
             // The "X-Accept-WebP" (value: "1"|"") header is set by Varnish and used in it's hash key. It's based
             // on whether the client's "Accept" header contained "image/webp" and is used for detecting that and
             // using a different image format within the HTML throughout the site.
-            $can_use_webp = (bool) Craft::$app->getRequest()->getHeaders()->get('X-Accept-WebP', false);
+            $can_use_webp = (bool) $request_headers->get('X-Accept-WebP', false);
 
             // Use browser type to work out which build to use (set by nginx proxy virtual host - before Varnish)
-            $browser_type_header = Craft::$app->getRequest()->getHeaders()->get('X-Browser-Type', 'modern');
+            $browser_type_header = $request_headers->get('X-Browser-Type', 'modern');
             if ($browser_type_header === 'legacy' || $browser_type_header === 'modern') {
                 $browser_type = $browser_type_header;
             }
