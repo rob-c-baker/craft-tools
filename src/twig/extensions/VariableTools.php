3902a5322fbe24f7dbc16943d8bbd96711640d68
Rob Baker <rob.baker@alanrogers.com>
2022-06-28 19:21:34 +0100

    `setKeyValue()` now supported nested keys and alters the Twig context, in effect, changing variables by reference. i.e. in most cases this will be the Twig code `{% do setKeyValue('variable_name', key, value) %}`

--- src/twig/extensions/VariableTools.php
+++ src/twig/extensions/VariableTools.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace alanrogers\tools\twig\extensions;
 
+use craft\helpers\ArrayHelper;
+use RuntimeException;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -19,7 +21,7 @@ class VariableTools extends AbstractExtension
             new TwigFunction(
                 'setKeyValue',
                 [ VariableTools::class, 'setKeyValue' ],
-                []
+                [ 'needs_context' => true ]
             ),
         ];
     }
@@ -41,18 +43,26 @@ class VariableTools extends AbstractExtension
     }
 
     /**
-     * @param mixed $target The object / array to set a key's value on (if not an array or object, this function is a noop)
-     * @param int|string $key the key to use
+     * @param mixed $context
+     * @param string $variable_name The object / array name to set a key's value on (if not an array or object, this function is a noop)
+     * @param int|string|array $key the key to use (can be a dot separated string to indicate nested keys, but only if the target is an array)
      * @param mixed $value the value to set
      * @return void
      */
-    public static function setKeyValue($target, $key, $value)
+    public static function setKeyValue(array &$context, string $variable_name, $key, $value) : void
     {
-        if (is_array($target)) {
-            $target[$key] = $value;
-        } elseif (is_object($target)) {
-            $target->$key = $value;
+        // we want to be sure we are altering a reference as we want to modify the variable in place
+        if (isset($context[$variable_name])) {
+            $target = &$context[$variable_name];
+            if (is_array($target)) {
+                // Using this helper means that we can have keys like "key1.key2.key3" referring to nested array values
+                // ...or a key like ['key1','key2','key3'] meaning the same thing
+                ArrayHelper::setValue($target, $key, $value);
+            } elseif (is_object($target)) {
+                $target->$key = $value;
+            }
+        } else {
+            throw new RuntimeException(sprintf('Cannot set a key on variable "%s" because it is undefined.', $variable_name));
         }
-        return $target;
     }
 }
