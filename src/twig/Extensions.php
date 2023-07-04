71f73330d448a58f3ea9452746f555ebaee7c544
Rob Baker <rob.baker@alanrogers.com>
2023-07-04 16:51:28 +0100

    Allow for Twig extensions to already be registered

--- src/twig/Extensions.php
+++ src/twig/Extensions.php
@@ -10,6 +10,7 @@ use Craft;
 use Exception;
 use alanrogers\tools\twig\extensions\DeepMerge;
 use alanrogers\tools\twig\extensions\Inline;
+use LogicException;
 use marionnewlevant\twigperversion\twigextensions\TwigPerversionTwigExtension;
 use Twig\TwigFilter;
 
@@ -20,29 +21,53 @@ class Extensions
         $view = Craft::$app->getView();
         $twig = $view->getTwig();
 
-        // Add our Twig filters
-        $twig->addFilter(new TwigFilter('is_array', function($value) {
-            return is_array($value);
-        }));
-        $twig->addFilter(new TwigFilter('array_values', function($value) {
-            return array_values($value);
-        }));
-        $twig->addFilter(new TwigFilter('intval', function($value) {
-            return intval($value);
-        }));
-        $twig->addFilter(new TwigFilter('push', function($value, $push1, $push2=null) {
-            if ($push2) {
-                // If there is a second param then  $push1 is the key and push2 is the value
-                $value[$push1] = $push2;
-            } else {
-                // If no second param, then $push1 is the value
-                $value[] = $push1;
-            }
-            return $value;
-        }));
-        $twig->addFilter(new TwigFilter('str_hash', function($value, string $algorithm, array $options=[]) {
-            return hash($algorithm, $value, false, $options);
-        }));
+        try {
+            // Add our Twig filters
+            $twig->addFilter(new TwigFilter('is_array', function($value) {
+                return is_array($value);
+            }));
+        } catch (LogicException $e) {
+            // might get here if for some reason they are already registered.
+        }
+        
+        try {
+            $twig->addFilter(new TwigFilter('array_values', function($value) {
+                return array_values($value);
+            }));
+        } catch (LogicException $e) {
+            // might get here if for some reason they are already registered.
+        }
+
+        try {
+            $twig->addFilter(new TwigFilter('intval', function($value) {
+                return intval($value);
+            }));
+        } catch (LogicException $e) {
+            // might get here if for some reason they are already registered.
+        }
+
+        try {
+            $twig->addFilter(new TwigFilter('str_hash', function($value, string $algorithm, array $options=[]) {
+                return hash($algorithm, $value, false, $options);
+            }));
+        } catch (LogicException $e) {
+            // might get here if for some reason they are already registered.
+        }
+
+        try {
+            $twig->addFilter(new TwigFilter('push', function($value, $push1, $push2=null) {
+                if ($push2) {
+                    // If there is a second param then  $push1 is the key and push2 is the value
+                    $value[$push1] = $push2;
+                } else {
+                    // If no second param, then $push1 is the value
+                    $value[] = $push1;
+                }
+                return $value;
+            }));
+        } catch (LogicException $e) {
+            // might get here if for some reason they are already registered.
+        }
 
         // Twig perversion plugin: https://github.com/marionnewlevant/craft-twig_perversion
         $view->registerTwigExtension(new TwigPerversionTwigExtension());
