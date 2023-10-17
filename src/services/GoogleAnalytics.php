677e8c98d50db6ac7723d8333d264a088e6ec173
Rob Baker <rob.baker@alanrogers.com>
2023-10-17 13:17:26 +0100

    BC: Switch Analytics reporting code to New GA4 data reporting (v1)

--- src/services/GoogleAnalytics.php
+++ src/services/GoogleAnalytics.php
@@ -2,77 +2,51 @@
 
 namespace alanrogers\tools\services;
 
-use DateTime;
-use Google\Client;
-use Google\Service\Analytics;
-use Google\Service\Analytics\GaData;
+use alanrogers\tools\exceptions\GAException;
+use Google\Analytics\Data\V1beta\BetaAnalyticsDataClient;
+use Google\Analytics\Data\V1beta\RunReportResponse;
+use Google\ApiCore\ApiException;
+use Google\ApiCore\ValidationException;
 use yii\base\Component;
 
 class GoogleAnalytics extends Component
 {
-    /**
-     * @var Client|null
-     */
-    private ?Client $google_client = null;
+    private ?BetaAnalyticsDataClient $client = null;
 
     /**
-     * @var Analytics|null
+     * @throws GAException
      */
-    private ?Analytics $analytics_service = null;
-
-    private function initGoogleClient() : void
+    public function getGoogleAnalyticsService(): BetaAnalyticsDataClient
     {
-        $this->google_client = new Client();
-
-        // Tell the Google client to use the credentials in the environment variable "GOOGLE_APPLICATION_CREDENTIALS"
-        $this->google_client->useApplicationDefaultCredentials();
-    }
-
-    private function initGoogleAnalyticsService() : void
-    {
-        // We are using Analytics (read only) here...
-        $this->getGoogleClient()->addScope(Analytics::ANALYTICS_READONLY);
-        $this->analytics_service = new Analytics($this->google_client);
-    }
-
-    /**
-     * @return Client
-     */
-    public function getGoogleClient() : Client
-    {
-        if ($this->google_client === null) {
-            $this->initGoogleClient();
+        if ($this->client === null) {
+            $credentials_path = $_SERVER['GOOGLE_APPLICATION_CREDENTIALS'] ?? null;
+            try {
+                $this->client = new BetaAnalyticsDataClient([
+                    'credentials' => json_decode(file_get_contents($credentials_path), true)
+                ]);
+            } catch (ValidationException $e) {
+                throw new GAException('Unable to create Analytics data client.', (int) $e->getCode(), $e);
+            }
         }
-        return $this->google_client;
+        return $this->client;
     }
 
     /**
-     * @return Analytics
+     * @param string $ga_property
+     * @param array $args
+     * @return RunReportResponse
+     * @throws GAException
+     * @see BetaAnalyticsDataGapicClient::runReport()
      */
-    public function getGoogleAnalyticsService() : Analytics
+    public function runReport(string $ga_property, array $args=[]): RunReportResponse
     {
-        if ($this->analytics_service === null) {
-            $this->initGoogleAnalyticsService();
+        try {
+            return $this->getGoogleAnalyticsService()->runReport([
+                ...$args,
+                'property' => 'properties/' . $ga_property
+            ]);
+        } catch (ApiException $e) {
+            throw new GAException('Unable to run Analytics report.', (int) $e->getCode(), $e);
         }
-        return $this->analytics_service;
-    }
-
-    /**
-     * @param string $report_id
-     * @param DateTime $start_date
-     * @param DateTime $end_date
-     * @param string $metrics A comma-separated list of Analytics metrics. E.g. 'ga:sessions,ga:pageviews'.
-     * @param array $params
-     * @return GaData
-     */
-    public function getGAData(string $report_id, DateTime $start_date, DateTime $end_date, string $metrics, array $params=[]) : GaData
-    {
-        return $this->getGoogleAnalyticsService()->data_ga->get(
-            $report_id,
-            $start_date->format('Y-m-d'),
-            $end_date->format('Y-m-d'),
-            $metrics,
-            $params
-        );
     }
 }
