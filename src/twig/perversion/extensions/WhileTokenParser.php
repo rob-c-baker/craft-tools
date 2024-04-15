f6cd9d0072862f00f69978dfc3e145ffbecde753
Rob Baker <rob.baker@alanrogers.com>
2024-04-15 14:53:27 +0100

    Bring "Twig Perversion" plugin into this library, with no install process - so we can remove the plugin that is not upgradable to Craft 5.

--- src/twig/perversion/extensions/WhileTokenParser.php
+++ src/twig/perversion/extensions/WhileTokenParser.php
@@ -2,7 +2,34 @@
 
 namespace alanrogers\tools\twig\perversion\extensions;
 
-class WhileTokenParser extends \Twig\TokenParser\AbstractTokenParser
-{
+use Twig\Token;
+use Twig\TokenParser\AbstractTokenParser;
 
+class WhileTokenParser extends AbstractTokenParser
+{
+    public function parse(Token $token): WhileNode
+    {
+        $lineno = $token->getLine();
+        $parser = $this->parser;
+        $stream = $parser->getStream();
+        $condition = $parser->getExpressionParser()->parseExpression();
+        $stream->expect(Token::BLOCK_END_TYPE);
+        $body = $parser->subparse([ $this, 'decideWhileEnd' ]);
+
+        $stream->next();
+
+        $stream->expect(Token::BLOCK_END_TYPE);
+
+        return new WhileNode($condition, $body, $lineno, $this->getTag());
+    }
+
+    public function decideWhileEnd(Token $token): bool
+    {
+        return $token->test([ 'endwhile' ]);
+    }
+
+    public function getTag(): string
+    {
+        return 'while';
+    }
 }
