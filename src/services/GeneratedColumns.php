b8f993a5344f9da36c93099b5f46a3e42f344dff
Rob Baker <rob.baker@alanrogers.com>
2024-09-19 11:48:09 +0100

    generated columns service - support getting fields from an asset volume.

--- src/services/GeneratedColumns.php
+++ src/services/GeneratedColumns.php
@@ -28,14 +28,39 @@ class GeneratedColumns
     {
         $entry_type = Craft::$app->getEntries()->getEntryTypeByHandle($entry_type);
         if ($entry_type === null) {
-            throw new GeneratedColumnException('Invalid entry type: ' . $entry_type);
+            throw new GeneratedColumnException(sprintf('Invalid entry type "%s". ', $entry_type));
         }
 
         $field_layout = $entry_type->getFieldLayout();
 
         $field = $field_layout->getFieldByHandle($field);
         if ($field === null) {
-            throw new GeneratedColumnException('Invalid field: ' . $field);
+            throw new GeneratedColumnException(sprintf('Invalid field "%s" on entry type "%s".', $field, $entry_type->handle));
+        }
+
+        return $field;
+    }
+
+    /**
+     * Used to get the field from a specific volume.
+     * The resulting `FieldInterface` can then be used to generate the value SQL via a call to
+     * `FieldInterface::getSqlValue()` on the returned field.
+     * @param string $volume_handle The handle for the `Volume`
+     * @param string $field The handle for the field on the volume.
+     * @throws GeneratedColumnException
+     */
+    public function getFieldFromAssetVolume(string $volume_handle, string $field): FieldInterface
+    {
+        $volume = Craft::$app->getVolumes()->getVolumeByHandle($volume_handle);
+        if ($volume === null) {
+            throw new GeneratedColumnException(sprintf('Invalid asset volume "%s".', $volume_handle));
+        }
+
+        $field_layout = $volume->getFieldLayout();
+
+        $field = $field_layout->getFieldByHandle($field);
+        if ($field === null) {
+            throw new GeneratedColumnException(sprintf('Invalid field "%s" asset volume "%s".', $field, $volume_handle));
         }
 
         return $field;
