4580f77aa6521a28b9a40a02af5459aac2b64156
Rob Baker <rob.baker@alanrogers.com>
2024-10-14 11:13:58 +0100

    New email validator - add possibility to check spoof email addresses

--- src/validator/validators/EmailDNSRFC.php
+++ src/validator/validators/EmailDNSRFC.php
@@ -5,6 +5,7 @@ namespace alanrogers\tools\validator\validators;
 use alanrogers\tools\validator\Base;
 use Egulias\EmailValidator\EmailValidator;
 use Egulias\EmailValidator\Validation\DNSCheckValidation;
+use Egulias\EmailValidator\Validation\Extra\SpoofCheckValidation;
 use Egulias\EmailValidator\Validation\MultipleValidationWithAnd;
 use Egulias\EmailValidator\Validation\RFCValidation;
 use InvalidArgumentException;
@@ -17,6 +18,7 @@ class EmailDNSRFC extends Base
 {
     public const string CHECK_RFC = 'RFC';
     public const string CHECK_DNS = 'DNS';
+    public const string CHECK_SPOOF = 'SPOOF';
 
     #[Override]
     protected function validate($value): bool
@@ -31,6 +33,7 @@ class EmailDNSRFC extends Base
             $checks[] = match ($check) {
                 self::CHECK_RFC => new RFCValidation(),
                 self::CHECK_DNS => new DNSCheckValidation(),
+                self::CHECK_SPOOF => new SpoofCheckValidation(),
                 default => throw new InvalidArgumentException('Invalid check: ' . json_encode($check)),
             };
         }
